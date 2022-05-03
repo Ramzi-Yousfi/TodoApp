a4849b0b17dc9d864ef3161324f15c7f2e064b27
ramzi <ramzi446@hotmail.com>
2022-05-03 18:59:36 +0200

    crd

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React  from 'react';
 import Form from './components/Form';
-import Items  from './components/Items';
+
 
 function App() {
     return (
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,43 +1,58 @@
-import { useState } from "react";
+import {useState} from "react";
 import Items from "./Items";
 import React from 'react'
 import {v4 as uuidv4} from 'uuid'
 
 export default function Form() {
-    const [dataArr,setDataArr] = useState([
-        {txt:"premire item" , id:uuidv4()},
-        {txt:"deuxieme item" , id:uuidv4()},
-        {txt:"troisieme item" , id:uuidv4()},
-        {txt:"quatrieme item" , id:uuidv4()},
-    ]);
+    const [dataArr,setDataArr] = useState([]);
+    const [stateInput,setStateInput] = useState([]);
 
     const deleteElement = id => {
-        console.log(id);
+        const filtedState = dataArr.filter(item => {
+            return item.id !== id;
+        })
+        setDataArr(filtedState)
     }
-  return (
-    <div className="m-auto px-4 col-12">
-        <form action="" className="mb-3">
-            <label htmlFor="todo" className="from-label mt-3">
-                chose a faire
-            </label>
-            <input type="text" className="form-control" id="todo"/>
-        </form>
-        <button className="btn btn-success">ajouter</button>
-
-        <h2>Listes des choses a faire : </h2>
-        <ul className="list-group">
-            {dataArr.map(item=>{
-                return(
-                    <Items
-                        txt={item.txt}
-                        key={item.id}
-                        id = {item.id}
-                        delFunc={deleteElement}
-                   
+    const addElement = e => {
+        e.preventDefault();
+        const newArr = [...dataArr]
+        const newTodo = {}
+        newTodo.txt = stateInput;
+        newTodo.id = uuidv4();
+        newArr.push(newTodo);
+        setDataArr(newArr);
+        setStateInput("");
+        console.log("hello")
+    }
+    const linkedInput = e => {
+        setStateInput(e)
+    }
+    return (
+        <div className="m-auto px-4 col-12">
+            <form onSubmit={e => addElement(e)} action="" className="mb-3">
+                <label htmlFor="todo" className="from-label mt-3">
+                    chose a faire
+                </label>
+                <input 
+                    value={stateInput}
+                    onInput={e => linkedInput(e.target.value)}
+                    type="text"
+                    className="form-control"
+                    id="todo"
+                    required
                     />
-                )
-            })}
-        </ul>
-    </div>
-  )
+                    <br/>
+                    <button className="btn btn-success">ajouter</button>
+            </form>
+            
+
+            <h2>Listes des choses a faire :
+            </h2>
+            <ul className="list-group">
+                {dataArr.map(item => {
+                    return (<Items txt={item.txt} key={item.id} id={item.id} delFunc={deleteElement}/>)
+                })}
+            </ul>
+        </div>
+    )
 }
